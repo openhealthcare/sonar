e1ac3387fede2b2a0bf239b7cdea4315b78cbd63
David Miller <David>
2012-09-24 17:14:37 +0100

    Searchin urls n' views

--- innovation/views.py
+++ innovation/views.py
@@ -1,7 +1,22 @@
 from django.template.defaultfilters import slugify
-
+from django.views.generic import TemplateView
 from models import Item
 from forms import ItemForm
+
+class Search(TemplateView):
+    template_name = 'search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Search, self).get_context_data(**kwargs)
+        context['term'] = term = self.request.GET.get('term', 'FTW')
+        innovats = Item.objects.filter(summary__contains=term)
+        context['innovats'] = innovats
+        if len(innovats) > 0:
+            innovated = True
+        else:
+            innovated = False
+        context['innovated'] = innovated
+        return context
 
 
 def new_innovation(request):
