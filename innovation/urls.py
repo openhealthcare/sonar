0f64d4cb2494637e037e9f04dbc7d90ca59a06bd
Ross Jones <rossdjones@gmail.com>
2012-09-24 13:33:39 +0100

    temp hack

--- innovation/urls.py
+++ innovation/urls.py
@@ -16,8 +16,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-import settings
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
