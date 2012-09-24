4dbf15d8373297e4047d6845794fb867fa947edb
Ross Jones <rossdjones@gmail.com>
2012-09-24 13:35:13 +0100

    temp hack

--- innovation/urls.py
+++ innovation/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf import settings
 
 admin.autodiscover()
 
