bcf66d96c6273030f06b958451c171a5aee0f949
Ross Jones <rossdjones@gmail.com>
2012-09-24 16:56:24 +0100

    Removing admin classes;

--- innovation/admin.py
+++ innovation/admin.py
@@ -4,8 +4,6 @@ from . import models
 
 
 admin.site.register(models.Item)
-admin.site.register(models.Evidence)
 admin.site.register(models.Role)
-admin.site.register(models.Specialisation)
 admin.site.register(models.Vote)
 
