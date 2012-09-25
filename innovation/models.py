a915cceecb6d5de0081a32c7293e6918f8775dfc
Sym Roe <sym.roe@talusdesign.co.uk>
2012-09-25 11:32:24 +0100

    Item permalink

--- innovation/models.py
+++ innovation/models.py
@@ -32,6 +32,12 @@ class Item(models.Model):
     def __unicode__(self):
         return self.title
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ('idea', (), {
+        'slug': self.slug
+        })
+    
 
 class Role(models.Model):
     name = models.CharField(max_length=255)
