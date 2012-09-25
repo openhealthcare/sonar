9893ad69a27ecca1d689584f949b7f8866f913af
Sym Roe <sym.roe@talusdesign.co.uk>
2012-09-25 11:28:05 +0100

    Item ModelAdmin and prepopulate slug

--- innovation/admin.py
+++ innovation/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(models.Item)
+admin.site.register(models.Item, ItemAdmin)
 admin.site.register(models.Role)
 admin.site.register(models.Vote)
 
